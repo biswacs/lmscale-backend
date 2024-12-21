142fe96577b3062726702bc0c3b6a0a7ebf2b9df
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-12-22 04:04:21 +0530

    removing rate limit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const auth = require("../middleware/auth");
-const rateLimiters = require("../middleware/rateLimiter");
 
 const router = express.Router();
 
-router.post("/register", rateLimiters.register, userController.register);
-router.post("/login", rateLimiters.login, userController.login);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 router.get("/profile", auth, userController.getProfile);
 
 module.exports = router;
