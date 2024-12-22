264bfa10c835affc1a8e71fe93be577f0f8cd702
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-12-22 19:45:07 +0530

    rate

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,19 +6,7 @@ const rateLimiters = require("../middleware/rateLimiter");
 const router = express.Router();
 
 router.post("/register", rateLimiters.register, userController.register);
-router.post(
-  "/login",
-  (req, res, next) => {
-    console.log("⏳ Login request received");
-    next();
-  },
-  rateLimiters.login,
-  (req, res, next) => {
-    console.log("✅ Passed rate limiter");
-    next();
-  },
-  userController.login
-);
+router.post("/login", rateLimiters.login, userController.login);
 router.get("/profile", auth, userController.getProfile);
 
 module.exports = router;
